b7c1db733211fa0ccbe680d21b07e029283fefca
kbabel971 <kevin.jordan.babel@gmail.com>
2025-11-18 10:57:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,44 @@
 import { WebSocketServer } from "ws";
 
+// Liste des joueurs connectés
+let players = []; 
+let nextId = 1;
+
+// Création du serveur WS
 const server = new WebSocketServer({ port: 8080 });
 
 server.on("connection", (socket) => {
-  console.log("Client connecté");
 
-  socket.on("message", (message) => {
-    console.log("Reçu :", message);
-    socket.send("Serveur a reçu : " + message);
+  // Crée un joueur
+  const player = {
+    id: nextId++,
+    socket: socket
+  };
+
+  // Ajoute à la liste
+  players.push(player);
+
+  console.log("Nouveau joueur connecté :", player.id);
+
+  // Envoie son ID au joueur
+  socket.send(JSON.stringify({
+    type: "welcome",
+    id: player.id
+  }));
+
+  // Quand un message arrive
+  socket.on("message", (msg) => {
+    console.log(`Message du joueur ${player.id} :`, msg.toString());
   });
 
+  // Quand il se déconnecte
   socket.on("close", () => {
-    console.log("Client déconnecté");
+    console.log("Déconnexion du joueur :", player.id);
+
+    players = players.filter(p => p.id !== player.id);
   });
 });
 
 console.log("Serveur WebSocket lancé sur le port 8080");
+
 
